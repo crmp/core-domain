6d08771b971cf4ff939da5fe1f8ea2031e4940a4
Mike Pretzlaw <pretzlaw@gmail.com>
2016-12-04 21:16:23 +0100

    Inquiry: Title and content

--- lib/Crmp/Domain/Inquiry.php
+++ lib/Crmp/Domain/Inquiry.php
@@ -14,13 +14,29 @@ class Inquiry {
 	 * @var Address[]
 	 */
 	protected $addresses;
+	/**
+	 * Description of the inquiry.
+	 *
+	 * @var string
+	 */
+	protected $content;
+	/**
+	 * Title of the inquiry.
+	 *
+	 * @var string
+	 */
+	protected $title;
 
 	/**
 	 * Create new inquiry.
 	 *
+	 * @param string    $title     Subject.
+	 * @param string    $content   Description.
 	 * @param Address[] $addresses Related addresses.
 	 */
-	public function __construct( $addresses = [] ) {
+	public function __construct( $title, $content, $addresses = [] ) {
+		$this->title     = $title;
+		$this->content   = $content;
 		$this->addresses = $addresses;
 	}
 
@@ -35,5 +51,23 @@ class Inquiry {
 	 */
 	public function getAddresses() {
 		return $this->addresses;
+	}
+
+	/**
+	 * Description.
+	 *
+	 * @return string
+	 */
+	public function getContent() {
+		return $this->content;
+	}
+
+	/**
+	 * Receive title.
+	 *
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
 	}
 }
--- lib/Crmp/Tests/UnitTests/Domain/InquiryTest.php
+++ lib/Crmp/Tests/UnitTests/Domain/InquiryTest.php
@@ -13,18 +13,33 @@ class InquiryTest extends \PHPUnit_Framework_TestCase {
 
 	public function testAddressesCanBeSet() {
 		$addresses = [ $this->createAddressStub(), $this->createAddressStub() ];
-		$inquiry   = new Inquiry( $addresses );
+		$inquiry   = new Inquiry( 'subject', 'content', $addresses );
 
 		$this->assertEquals( $addresses, $inquiry->getAddresses() );
 	}
 
 	public function testItCanAppendAddresses() {
-		$inquiry = new Inquiry();
+		$inquiry = new Inquiry( 'subject', 'content' );
 
 		$address = $this->createAddressStub();
 
 		$inquiry->appendAddress( $address );
 
 		$this->assertEquals( [ $address ], $inquiry->getAddresses() );
+	}
+
+	public function testItHasAContent() {
+		$content = uniqid();
+
+		$inquiry = new Inquiry( 'subject', $content );
+
+		$this->assertEquals( $content, $inquiry->getContent() );
+	}
+
+	public function testItHasATitle() {
+		$title   = uniqid();
+		$inquiry = new Inquiry( $title, 'content' );
+
+		$this->assertEquals( $title, $inquiry->getTitle() );
 	}
 }
